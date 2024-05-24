e147275917d05ef25251f853b9dff5e260e605f9
Justin <165220142+DeltaV09@users.noreply.github.com>
2024-05-24 11:40:37 -0500

    Add function to get Human Choice

--- javascript.js
+++ javascript.js
@@ -3,18 +3,29 @@ function getComputerChoice () {
     console.log(ranNumber);
     switch (ranNumber) {                               //returns Rock/Paper/Scissors based on ranNumber
         case 1:
-            return "Rock";
+            return "rock";
             break;
         case 2:
-            return "Paper";
+            return "paper";
             break;
         case 3:
-            return "Scissors";
+            return "scissors";
             break;
     }
 }
 
-let test = getComputerChoice();
-console.log(test);
+function getHumanChoice () {        //get the Player's choice
+    let humanChoice = prompt("Choose Rock, Paper, or Scissors");
+    humanChoice = humanChoice.toLowerCase();  //convert choice to lower case for comparison
+    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+        return humanChoice;             //return player choice if valid
+    }
+    else {
+        return "invalid input";         //return "invalid input" if player does not enter rock, paper, or scissors
+    }
 
+}
+
+let test = getHumanChoice();
+console.log(test);
 
