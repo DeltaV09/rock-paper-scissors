abd3a6092c02a0732300f392bd066fb9cce50c07
Justin <165220142+DeltaV09@users.noreply.github.com>
2024-05-24 13:45:09 -0500

    Add function to play to Best of 5 rounds* *Really play to first to three because players stop once game is unwinnable, and ties are not counted

--- javascript.js
+++ javascript.js
@@ -24,42 +24,54 @@ function getHumanChoice () {        //get the Player's choice
     return humanChoice;
 }
 
-function playRound (humanChoice, computerChoice) {                      //plays one round
-    if (humanChoice === "rock" && computerChoice === "paper") {
-        alert("You lose! Paper beats Rock!");
-        computerScore++;
+
+
+function playGame () {      //play Best of 5 (but ties don't count, so really play until someone has score of 3)
+    let humanScore = 0;
+    let computerScore = 0;
+    while (humanScore <3 && computerScore < 3) {
+        playRound(getHumanChoice(), getComputerChoice());
     }
-    else if (humanChoice === "rock" && computerChoice === "scissors") {
-        alert("You win! Rock beats Scissors!");
-        humanScore++;
-    }  
-    else if (humanChoice === "paper" && computerChoice === "rock") {
-        alert("You win! Paper beats Rock!");
-        humanScore++;
+
+    if (humanScore === 3) {      //declare winner once someone has score of 3         
+        alert("Congratulations! You are the winner!");
     }
-    else if (humanChoice === "paper" && computerChoice === "scissors") {
-        alert("You lose! Scissors beats Paper!");
-        computerScore++;
+    else if (computerScore === 3) {
+        alert("Sorry, better luck next time!");
     }
-    else if (humanChoice === "scissors" && computerChoice === "rock") {
-        alert("You lose! Rock beats Scissors!");
-        computerScore++;
-    }
-    else if (humanChoice === "scissors" && computerChoice === "paper") {
-        alert("You win! Scissors beat Paper!");
-        humanScore++;
-    }
-    else {
-        alert("Tie!");
+
+    function playRound (humanChoice, computerChoice) {                      //plays one round
+        if (humanChoice === "rock" && computerChoice === "paper") {         //evaluates winner of round, increments score
+            alert("You lose! Paper beats Rock!");
+            computerScore++;
+        }
+        else if (humanChoice === "rock" && computerChoice === "scissors") {
+            alert("You win! Rock beats Scissors!");
+            humanScore++;
+        }  
+        else if (humanChoice === "paper" && computerChoice === "rock") {
+            alert("You win! Paper beats Rock!");
+            humanScore++;
+        }
+        else if (humanChoice === "paper" && computerChoice === "scissors") {
+            alert("You lose! Scissors beats Paper!");
+            computerScore++;
+        }
+        else if (humanChoice === "scissors" && computerChoice === "rock") {
+            alert("You lose! Rock beats Scissors!");
+            computerScore++;
+        }
+        else if (humanChoice === "scissors" && computerChoice === "paper") {
+            alert("You win! Scissors beat Paper!");
+            humanScore++;
+        }
+        else {
+            alert("Tie!");
+        }
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
+playGame();
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-console.log(humanSelection, computerSelection);
-playRound(humanSelection, computerSelection);
 
 
